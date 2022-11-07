ecf96fb52639ad2f45fdea818df5f02f2d00a68c
James Wallis <j@wallis.dev>
2022-11-07 18:39:43 +0000

    feat: Fix Jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,21 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "coverage",
+        outputName: "jest-junit.xml",
+        ancestorSeparator: " › ",
+        uniqueOutputName: "false",
+        suiteNameTemplate: "{filepath}",
+        classNameTemplate: "{classname}",
+        titleTemplate: "{title}",
+      },
+    ],
+  ],
   transform: {
     // Using a babel.config.js file disables the Next.js SWC replacement for babel
     // https://github.com/vercel/next.js/issues/30811#issuecomment-963102661
