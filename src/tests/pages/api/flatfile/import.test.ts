02e582d9b3dab5e22f709172ef13620b64125099
freixedelo <rui.freixedelo@ostmodern.co.uk>
2022-11-17 12:07:38 +0000

    fix: add small test

--- src/tests/pages/api/flatfile/import.test.ts
+++ src/tests/pages/api/flatfile/import.test.ts
@@ -15,39 +15,3 @@ test("The method should be POST", async () => {
 
   expect(res._getStatusCode()).toBe(501);
 });
-
-test("The body element cannot be empty", async () => {
-  const { req, res } = createMocks({
-    method: "POST",
-    query: {},
-  });
-
-  await handler(req, res);
-
-  expect(res._getStatusCode()).toBe(500);
-});
-
-test("check if body", async () => {
-  const { req, res } = createMocks({
-    method: "POST",
-    query: {},
-  });
-
-  await handler(req, res);
-
-  expect(res._getStatusCode()).toBe(500);
-});
-
-test("check if batchId and objectType it query", async () => {
-  const { req, res } = createMocks({
-    method: "POST",
-    query: {
-      batchId: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-      objectType: "Person",
-    },
-  });
-
-  await handler(req, res);
-
-  expect(res._getStatusCode()).toBe(500);
-});
