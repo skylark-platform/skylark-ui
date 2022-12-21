73baf4216083269afdbc1b9a8c9a5596f01d1e85
James Wallis <j@wallis.dev>
2022-12-21 12:43:18 +0000

    fix: disable swcMinify in next config as it breaks react-table

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false, // https://github.com/TanStack/table/issues/4610
 };
 
 module.exports = nextConfig;
