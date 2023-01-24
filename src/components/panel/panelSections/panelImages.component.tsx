dbb742804c66b97116c5282b93786eb143c15f4b
freixedelo <freixedelo@users.noreply.github.com>
2023-01-24 10:51:53 +0000

    Section images on panel by type (#24)
    * images on panel
    
    * fix: add content_type to image object

--- src/components/panel/panelSections/panelImages.component.tsx
+++ src/components/panel/panelSections/panelImages.component.tsx
@@ -6,33 +6,67 @@ export const PanelImages = ({
 }: {
   images: SkylarkGraphQLObjectImage[];
 }) => {
+  const imgs = images.reduce(
+    (acc: { [key: string]: SkylarkGraphQLObjectImage[] }, currentValue) => {
+      if (acc && acc[currentValue.type])
+        return {
+          ...acc,
+          [currentValue.type]: [...acc[currentValue.type], currentValue],
+        };
+      return {
+        ...acc,
+        [currentValue.type]: [currentValue],
+      };
+    },
+    {},
+  );
+
   return (
     <div className=" h-full overflow-y-scroll p-4 pb-12 text-sm md:p-8">
-      {images.map((image) => (
-        <div key={image.uid}>
-          <h3 className="mb-2 font-bold">{formatObjectField(image.type)}</h3>
-          <div className="flex flex-row">
-            <div className="w-1/2">
-              Title: {formatObjectField(image.title)}
-              Description: {formatObjectField(image.description)}
-            </div>
-            <div className="w-1/2">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                className="max-h-72 w-full object-cover"
-                src={image.url}
-                alt={
-                  image.title ||
-                  image.description ||
-                  `Preview for ${
-                    image.url || image.slug || image.external_id || image.uid
-                  }`
-                }
-              />
-            </div>
-          </div>
-        </div>
-      ))}
+      <h2 className="mb-4 text-xl font-bold">Imagery</h2>
+      {Object.keys(imgs).map((type) => {
+        return (
+          <>
+            <h3 className="mt-2 mb-4 font-bold">
+              {formatObjectField(type)} ({imgs[type].length})
+            </h3>
+            {imgs[type].map((image) => (
+              <div key={image.uid} className="pb-4">
+                <div className="flex flex-row">
+                  <div className="w-1/2 pr-4">
+                    <div className="flex flex-row pb-1">
+                      <div className="w-1/2">title: </div>
+                      <div className="w-1/2">
+                        {formatObjectField(image.title) || "---"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row pb-1">
+                      <div className="w-1/2">content_type: </div>
+                      <div className="w-1/2">
+                        {formatObjectField(image.content_type) || "---"}
+                      </div>
+                    </div>
+                    <div className="flex flex-row pb-1">
+                      <div className="w-1/2">description: </div>
+                      <div className="w-1/2">
+                        {formatObjectField(image.description) || "---"}
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" w-1/2">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      className="m-auto max-h-64"
+                      src={image.url}
+                      alt="Picture of the author"
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </>
+        );
+      })}
     </div>
   );
 };
