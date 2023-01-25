aec3d58ed2ef293d863dc8bc13444ca8898c1e1e
James Wallis <j@wallis.dev>
2023-01-25 17:25:02 +0000

    fix: cypress

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -19,14 +19,14 @@ describe("Navigation", () => {
 
   it("contains the navigation bar", () => {
     cy.contains("Skylark");
-    cy.get("nav").get("ul").find("li a").should("have.length", 2);
+    cy.get("nav").get("ul").find("li").should("have.length", 2);
     cy.percySnapshot("Navigation - desktop");
   });
 
   it("opens the navigation bar on mobile", () => {
     cy.viewport("iphone-xr");
     cy.get("#mobile-nav-toggle").click();
-    cy.get("nav").get("ul").find("li a").should("have.length", 2);
+    cy.get("nav").get("ul").find("li").should("have.length", 2);
     cy.percySnapshot("Navigation - mobile");
   });
 });
