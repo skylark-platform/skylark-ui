8c8bdc872aca513e50c039cc285056b5708e2f19
freixedelo <rui.freixedelo@ostmodern.co.uk>
2023-03-07 11:21:24 +0000

    adding overflow anywhere

--- src/components/panel/panelSections/panelMetadata.component.tsx
+++ src/components/panel/panelSections/panelMetadata.component.tsx
@@ -66,7 +66,7 @@ export const PanelMetadata = ({ metadata, objectType }: PanelMetadataProps) => {
     : Object.keys(metadata);
 
   return (
-    <div className="h-full overflow-y-auto p-4 pb-12 text-sm md:p-8 md:pb-20">
+    <div className="overflow-anywhere h-full overflow-y-auto p-4 pb-12 text-sm md:p-8 md:pb-20">
       {metadata &&
         metadataProperties.map(
           (property) =>
