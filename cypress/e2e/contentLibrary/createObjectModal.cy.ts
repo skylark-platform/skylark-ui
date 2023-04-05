42312a1b759046a4b20f4f8495f2de1fc555401f
James Wallis <j@wallis.dev>
2023-04-05 13:26:09 +0100

    feat: Add Cypress tests

---
+++ cypress/e2e/contentLibrary/createObjectModal.cy.ts
@@ -0,0 +1,120 @@
+import {
+  hasMatchingQuery,
+  hasMatchingVariable,
+  hasOperationName,
+} from "../../support/utils/graphqlTestUtils";
+
+describe("Content Library - Metadata Panel", () => {
+  beforeEach(() => {
+    cy.login();
+
+    cy.intercept("POST", Cypress.env("skylark_graphql_uri"), (req) => {
+      if (hasOperationName(req, "IntrospectionQuery")) {
+        req.alias = "introspectionQuery";
+        req.reply({
+          fixture: "./skylark/queries/introspection/introspectionQuery.json",
+        });
+      }
+      if (hasOperationName(req, "GET_SKYLARK_SCHEMA")) {
+        req.reply({
+          fixture: "./skylark/queries/introspection/schema.json",
+        });
+      }
+      if (hasOperationName(req, "GET_Episode")) {
+        req.reply({
+          fixture: "./skylark/queries/getObject/gots01e01.json",
+        });
+      }
+      if (hasOperationName(req, "SEARCH")) {
+        req.reply({
+          fixture: "./skylark/queries/search/gotPage1.json",
+        });
+      }
+      if (hasOperationName(req, "CREATE_OBJECT_Episode")) {
+        req.alias = "createObject";
+        req.reply({
+          data: {
+            createObject: {
+              __typename: "Episode",
+              _config: { primary_field: "title", colour: "#ff7ba8" },
+              _meta: {
+                available_languages: ["en-GB", "pt-PT"],
+                language_data: { language: "en-GB", version: 1 },
+                global_data: { version: 2 },
+              },
+              uid: "01GX396FF5MFFZ5N8YYPAM4W03",
+              external_id: "recAZhG0fPFgamb8I",
+              slug: "winter-is-coming",
+              synopsis_long:
+                "North of the Seven Kingdoms of Westeros, Night's Watch soldiers are attacked by supernatural White Walkers. One soldier escapes but is captured at Castle Winterfell. Eddard \"Ned\" Stark, Warden of the North, executes him for desertion. Later, six orphaned dire wolf pups are found and one given to each Stark sibling, including Ned's bastard son, Jon Snow. In King's Landing, the Seven Kingdoms capital, Jon Arryn, the Hand of the King, dies suddenly. King Robert Baratheon, Ned's old friend, travels to Winterfell to recruit Ned and propose a marriage between his heir Joffrey and Ned's daughter, Sansa. Lysa Arryn, Jon's widow, sends her sister (Ned's wife), Catelyn, a letter claiming the Lannisters, Queen Cersei's family, murdered Arryn. Catelyn believes the Lannisters are now plotting against King Robert. Ned's young son, Brandon, climbs a tall tower and witnesses Cersei and her twin brother, Jaime Lannister, inside having sex. To hide their incest, Jaime pushes Bran from the high window. Across the Narrow Sea in Essos, exiled Prince Viserys Targaryen forces his sister, Daenerys, to marry the Dothraki warlord, Drogo, in exchange for an army to conquer Westeros and reclaim the Iron Throne. Robert Baratheon became king after Jaime Lannister killed \"Mad\" King Aerys Targaryen, earning Jaime the nickname, \"The King Slayer\". The ancient Targaryens once commanded dragons, and Daenerys is given three fossilized dragon eggs as a wedding gift.",
+              synopsis_medium:
+                "Series Premiere. Eddard Stark is torn between his family and an old friend when asked to serve at the side of King Robert Baratheon; Viserys plans to wed his sister to a nomadic warlord in exchange for an army.",
+              synopsis_short: null,
+              title: "GOT S01E1 - Winter is Coming",
+              title_long: null,
+              title_medium: null,
+              title_short: "Winter is Coming",
+              episode_number: 1,
+              release_date: "2011-04-17",
+            },
+          },
+        });
+      }
+    });
+
+    cy.visit("/");
+    cy.wait("@introspectionQuery");
+  });
+
+  const openModal = () => {
+    cy.contains("button", "Create").click();
+    cy.contains("button", "Create Object").click();
+    cy.contains("Select Object Type to get started.");
+  };
+
+  it("opens the create object modal", () => {
+    openModal();
+
+    cy.percySnapshot("Homepage - create object modal");
+  });
+
+  it("selects an object type", () => {
+    openModal();
+
+    cy.get("[data-testid=create-object-modal]").within(() => {
+      cy.get("[data-testid=select]").click();
+      cy.get("[data-testid=select-options]").within(() => {
+        cy.contains("Episode").click();
+      });
+
+      cy.contains("button", "Create Episode").should("be.disabled");
+
+      cy.percySnapshot("Homepage - create object modal - object type selected");
+    });
+  });
+
+  it("creates an object which opens the Panel", () => {
+    openModal();
+
+    cy.get("[data-testid=create-object-modal]").within(() => {
+      cy.get("[data-testid=select]").click();
+      cy.get("[data-testid=select-options]").within(() => {
+        cy.contains("Episode").click();
+      });
+
+      cy.contains("button", "Create Episode").should("be.disabled");
+
+      cy.getByLabel("Slug").type("got-s01e01");
+      cy.contains("button", "Create Episode").should("be.enabled").click();
+    });
+
+    cy.wait("@createObject");
+
+    // Check Create Modal closes
+    cy.get("[data-testid=create-object-modal]").should("not.exist");
+
+    // Check Panel opens
+    cy.contains("Metadata");
+    cy.getByLabel("Slug").should("have.value", "winter-is-coming");
+  });
+});
--- cypress/e2e/contentLibrary/objectPanel.cy.ts
+++ cypress/e2e/contentLibrary/objectPanel.cy.ts
@@ -7,7 +7,7 @@ import {
 const assetOnlyQuery =
   "query SEARCH($ignoreAvailability: Boolean = true, $queryString: String!) {\n  search(\n    ignore_availability: $ignoreAvailability\n    query: $queryString\n    limit: 1000\n  ) {\n    __typename\n    objects {\n      ... on Asset {\n        __typename\n        uid\n        external_id\n        __Asset__slug: slug\n        __Asset__title: title\n        __Asset__type: type\n        __Asset__url: url\n      }\n      __typename\n    }\n  }\n}";
 
-describe("Content Library - Metadata Panel", () => {
+describe("Content Library - Object Panel", () => {
   beforeEach(() => {
     cy.login();
 
@@ -81,13 +81,43 @@ describe("Content Library - Metadata Panel", () => {
           fixture: "./skylark/mutations/updateHomepageSetContent.json",
         });
       }
+      if (hasOperationName(req, "UPDATE_OBJECT_METADATA_Episode")) {
+        req.alias = "updateEpisodeMetadata";
+        req.reply({
+          data: {
+            updateObjectMetadata: {
+              __typename: "Episode",
+              _config: { primary_field: "title", colour: "#ff7ba8" },
+              _meta: {
+                available_languages: ["en-GB", "pt-PT"],
+                language_data: { language: "en-GB", version: 1 },
+                global_data: { version: 2 },
+              },
+              uid: "01GX396FF5MFFZ5N8YYPAM4W03",
+              external_id: "recAZhG0fPFgamb8I",
+              slug: "winter-is-coming",
+              synopsis_long:
+                "North of the Seven Kingdoms of Westeros, Night's Watch soldiers are attacked by supernatural White Walkers. One soldier escapes but is captured at Castle Winterfell. Eddard \"Ned\" Stark, Warden of the North, executes him for desertion. Later, six orphaned dire wolf pups are found and one given to each Stark sibling, including Ned's bastard son, Jon Snow. In King's Landing, the Seven Kingdoms capital, Jon Arryn, the Hand of the King, dies suddenly. King Robert Baratheon, Ned's old friend, travels to Winterfell to recruit Ned and propose a marriage between his heir Joffrey and Ned's daughter, Sansa. Lysa Arryn, Jon's widow, sends her sister (Ned's wife), Catelyn, a letter claiming the Lannisters, Queen Cersei's family, murdered Arryn. Catelyn believes the Lannisters are now plotting against King Robert. Ned's young son, Brandon, climbs a tall tower and witnesses Cersei and her twin brother, Jaime Lannister, inside having sex. To hide their incest, Jaime pushes Bran from the high window. Across the Narrow Sea in Essos, exiled Prince Viserys Targaryen forces his sister, Daenerys, to marry the Dothraki warlord, Drogo, in exchange for an army to conquer Westeros and reclaim the Iron Throne. Robert Baratheon became king after Jaime Lannister killed \"Mad\" King Aerys Targaryen, earning Jaime the nickname, \"The King Slayer\". The ancient Targaryens once commanded dragons, and Daenerys is given three fossilized dragon eggs as a wedding gift.",
+              synopsis_medium:
+                "Series Premiere. Eddard Stark is torn between his family and an old friend when asked to serve at the side of King Robert Baratheon; Viserys plans to wed his sister to a nomadic warlord in exchange for an army.",
+              synopsis_short: null,
+              title: "GOT S01E1 - Winter is Coming",
+              title_long: null,
+              title_medium: null,
+              title_short: "Winter is Coming",
+              episode_number: 1,
+              release_date: "2011-04-17",
+            },
+          },
+        });
+      }
     });
 
     cy.visit("/");
     cy.wait("@introspectionQuery");
   });
 
-  it("open Metadata panel", () => {
+  it("open Panel", () => {
     cy.get('input[name="search-query-input"]').type("got winter is coming");
     cy.contains("tr", "GOT S01E1 - Winter");
     cy.contains("tr", "GOT S01E1 - Winter")
@@ -106,7 +136,7 @@ describe("Content Library - Metadata Panel", () => {
     cy.percySnapshot("Homepage - metadata panel - fields");
   });
 
-  it("close Metadata panel", () => {
+  it("close Panel", () => {
     cy.get('input[name="search-query-input"]').type("got winter is coming");
     cy.contains("tr", "GOT S01E1 - Winter");
     cy.contains("tr", "GOT S01E1 - Winter")
@@ -139,41 +169,102 @@ describe("Content Library - Metadata Panel", () => {
       cy.get("[data-testid=graphql-query-modal-button]").parent().click();
     });
     cy.contains("Query for");
-    cy.percySnapshot("Homepage - metadata panel - graphql query");
+    cy.percySnapshot("Homepage - object panel - graphql query");
   });
 
-  it("change language to pt-PT", () => {
-    cy.get('input[name="search-query-input"]').type("got winter is coming");
-    cy.contains("tr", "GOT S01E1 - Winter");
-    cy.contains("tr", "en-GB")
-      .should(($el) => {
-        // eslint-disable-next-line jest/valid-expect
-        expect(Cypress.dom.isDetached($el)).to.eq(false);
-      })
-      .within(() => {
-        cy.get('[aria-label="object-info"]').click();
+  describe("Metadata tab", () => {
+    it("change language to pt-PT", () => {
+      cy.get('input[name="search-query-input"]').type("got winter is coming");
+      cy.contains("tr", "GOT S01E1 - Winter");
+      cy.contains("tr", "en-GB")
+        .should(($el) => {
+          // eslint-disable-next-line jest/valid-expect
+          expect(Cypress.dom.isDetached($el)).to.eq(false);
+        })
+        .within(() => {
+          cy.get('[aria-label="object-info"]').click();
+        });
+
+      cy.contains("Metadata");
+
+      cy.get("[data-testid=panel-metadata]").within(() => {
+        cy.getByLabel("Slug").should("have.value", "winter-is-coming");
       });
 
-    cy.contains("Metadata");
+      // Change language
+      cy.get("[data-testid=panel-header]").within(() => {
+        cy.contains("en-GB").click();
+        cy.contains("pt-PT").click();
+      });
 
-    cy.get("[data-testid=panel-metadata]").within(() => {
+      cy.get("[data-testid=panel-metadata]").within(() => {
+        cy.getByLabel("Title short").should(
+          "have.value",
+          "O Inverno Está Chegando",
+        );
+      });
+
+      cy.percySnapshot("Homepage - object panel - fields - pt-PT");
+    });
+
+    it("edit metadata and cancel", () => {
+      cy.get('input[name="search-query-input"]').type("got winter is coming");
+      cy.contains("tr", "GOT S01E1 - Winter");
+      cy.contains("tr", "en-GB")
+        .should(($el) => {
+          // eslint-disable-next-line jest/valid-expect
+          expect(Cypress.dom.isDetached($el)).to.eq(false);
+        })
+        .within(() => {
+          cy.get('[aria-label="object-info"]').click();
+        });
+
+      cy.contains("Metadata");
+      cy.contains("button", "Edit Metadata").should("not.be.disabled");
+
       cy.getByLabel("Slug").should("have.value", "winter-is-coming");
+      cy.getByLabel("Slug").clear().type("gots01e01_winter-is-coming");
+      cy.getByLabel("Slug").should("have.value", "gots01e01_winter-is-coming");
+      cy.get("[data-testid=panel-header]").within(() => {
+        cy.contains("button", "Save").should("not.be.disabled");
+        cy.contains("button", "Cancel").should("not.be.disabled");
+      });
+      cy.contains("Editing");
+
+      cy.percySnapshot("Homepage - object panel - fields (edit)");
+
+      // Test cancel
+      cy.contains("Cancel").click();
+      cy.getByLabel("Slug").should("have.value", "winter-is-coming");
+      cy.contains("Editing").should("not.exist");
+      cy.contains("button", "Edit Metadata").should("not.be.disabled");
     });
 
-    // Change language
-    cy.get("[data-testid=panel-header]").within(() => {
-      cy.contains("en-GB").click();
-      cy.contains("pt-PT").click();
-    });
+    it("edit metadata and save", () => {
+      cy.get('input[name="search-query-input"]').type("got winter is coming");
+      cy.contains("tr", "GOT S01E1 - Winter");
+      cy.contains("tr", "en-GB")
+        .should(($el) => {
+          // eslint-disable-next-line jest/valid-expect
+          expect(Cypress.dom.isDetached($el)).to.eq(false);
+        })
+        .within(() => {
+          cy.get('[aria-label="object-info"]').click();
+        });
 
-    cy.get("[data-testid=panel-metadata]").within(() => {
-      cy.getByLabel("Title short").should(
-        "have.value",
-        "O Inverno Está Chegando",
-      );
-    });
+      cy.contains("Metadata");
+      cy.contains("button", "Edit Metadata").should("not.be.disabled");
 
-    cy.percySnapshot("Homepage - metadata panel - fields - pt-PT");
+      cy.getByLabel("Slug").should("have.value", "winter-is-coming");
+      cy.getByLabel("Slug").clear().type("gots01e01_winter-is-coming");
+      cy.getByLabel("Slug").should("have.value", "gots01e01_winter-is-coming");
+
+      cy.contains("Save").click();
+
+      cy.wait("@updateEpisodeMetadata");
+      cy.contains("Editing").should("not.exist");
+      cy.contains("button", "Edit Metadata").should("not.be.disabled");
+    });
   });
 
   describe("Imagery tab", () => {
@@ -200,7 +291,7 @@ describe("Content Library - Metadata Panel", () => {
           "https://dl.airtable.com/.attachments/c8b23d45c55cf09081954dd208dcce4b/80b72296/GOT-S1-1.jpg",
         );
 
-      cy.percySnapshot("Homepage - metadata panel - imagery");
+      cy.percySnapshot("Homepage - object panel - imagery");
     });
   });
 
@@ -218,7 +309,7 @@ describe("Content Library - Metadata Panel", () => {
         });
       cy.contains("button", "Content").click();
 
-      cy.percySnapshot("Homepage - metadata panel - content");
+      cy.percySnapshot("Homepage - object panel - content");
     });
 
     it("Reorder, remove and cancel", () => {
@@ -256,7 +347,7 @@ describe("Content Library - Metadata Panel", () => {
           "Discover Collection",
         ]);
 
-      cy.percySnapshot("Homepage - metadata panel - content (edit)");
+      cy.percySnapshot("Homepage - object panel - content (edit)");
 
       // Test deleting
       cy.contains("Discover Collection")
@@ -290,7 +381,7 @@ describe("Content Library - Metadata Panel", () => {
           "Home page hero",
         ]);
 
-      cy.percySnapshot("Homepage - metadata panel - content (reorder)");
+      cy.percySnapshot("Homepage - object panel - content (reorder)");
 
       // Test cancel
       cy.contains("Cancel").click();
@@ -398,7 +489,7 @@ describe("Content Library - Metadata Panel", () => {
       cy.contains("Time Window");
       cy.contains("Audience");
 
-      cy.percySnapshot("Homepage - metadata panel - availability");
+      cy.percySnapshot("Homepage - object panel - availability");
     });
   });
 });
