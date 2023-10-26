9a6b9c038610301bade7c3d8ed57b0a94534635f
James Wallis <j@wallis.dev>
2023-10-26 14:48:26 +0100

    chore: cr

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,37 +2,23 @@ const path = require("path");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    // {
-    //   name: "storybook-addon-next",
-    //   options: {
-    //     nextConfigPath: path.resolve(__dirname, "../next.config.js"),
-    //   },
-    // },
   ],
-
   framework: {
     name: "@storybook/nextjs", // Add this
     options: {
       nextConfigPath: path.resolve(__dirname, "../next.config.js"),
     },
   },
-
-  // core: {
-  //   builder: "webpack5",
-  // },
   staticDirs: ["../public"],
-
   env: (config) => ({
     ...config,
     NEXT_PUBLIC_SAAS_API_ENDPOINT: "http://localhost:6006",
     NEXT_PUBLIC_SAAS_API_KEY: "token",
   }),
-
   docs: {
     autodocs: true,
   },
