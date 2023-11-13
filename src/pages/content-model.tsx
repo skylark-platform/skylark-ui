91f0d307f205305585b54af86809294f00c7f349
James Wallis <j@wallis.dev>
2023-11-13 11:47:29 +0000

    feat: add warning

--- src/pages/content-model.tsx
+++ src/pages/content-model.tsx
@@ -3,7 +3,16 @@ import { ContentModel } from "src/components/contentModel";
 export default function ContentModelPage() {
   return (
     <div className="pt-nav">
-      <ContentModel />
+      <div className="fixed z-[10000] w-full bg-warning flex justify-center items-center text-sm flex-col h-14">
+        <p>This is an Alpha feature.</p>
+        <p>
+          Currently, you can only modify UI Config. Schema modifications are
+          unsupported.
+        </p>
+      </div>
+      <div className="pt-14">
+        <ContentModel />
+      </div>
     </div>
   );
 }
