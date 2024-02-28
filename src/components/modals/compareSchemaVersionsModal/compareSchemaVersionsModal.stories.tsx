6e567f772a1006e099f6e6a1f0bff2f9cb3d3e1e
James Wallis <j@wallis.dev>
2024-02-28 18:32:31 +0100

    fix: storybook

--- src/components/modals/compareSchemaVersionsModal/compareSchemaVersionsModal.stories.tsx
+++ src/components/modals/compareSchemaVersionsModal/compareSchemaVersionsModal.stories.tsx
@@ -1,9 +1,23 @@
+import type { Meta } from "@storybook/react";
 import { userEvent, screen } from "@storybook/testing-library";
 
 import { CompareSchemaVersionsModal } from "./compareSchemaVersionsModal.component";
 
-export default { component: CompareSchemaVersionsModal };
-export const Default = {
+const meta: Meta<typeof CompareSchemaVersionsModal> = {
+  component: CompareSchemaVersionsModal,
+  // Decorator to increase Story height https://www.chromatic.com/docs/snapshots#why-are-components-that-render-in-a-portal-tooltip-modal-menu-ge
+  decorators: [
+    (StoryComponent) => (
+      <div className="h-screen w-screen">
+        <StoryComponent />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+export const Default: Meta<typeof CompareSchemaVersionsModal> = {
   args: {
     isOpen: true,
     baseVersionNumber: 1,
