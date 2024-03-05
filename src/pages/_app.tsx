b8fce7aeedfb88f0d74e654f8912ee98d03a07e0
James Wallis <j@wallis.dev>
2024-03-05 11:39:35 +0100

    feat: Add intercom walkthrough

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ import Head from "next/head";
 import Script from "next/script";
 import { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
+import { IntercomProvider } from "react-use-intercom";
 
 import { Navigation } from "src/components/navigation";
 import { ToastContainer } from "src/components/toast/toast.component";
@@ -25,50 +26,58 @@ const loadFramerMotionFeatures = () =>
     (res) => res.default,
   );
 
+const intercomAppId = process.env.NEXT_PUBLIC_INTERCOM_APP_ID;
+
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(createSkylarkReactQueryClient);
 
   return (
     <PlausibleProvider domain={APP_URL} enabled={!!APP_URL}>
-      <QueryClientProvider client={queryClient}>
-        <ToastContainer />
-        <Head>
-          <title>Skylark</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
+      <IntercomProvider
+        appId={intercomAppId || ""}
+        autoBoot
+        shouldInitialize={!!intercomAppId}
+      >
+        <QueryClientProvider client={queryClient}>
+          <ToastContainer />
+          <Head>
+            <title>Skylark</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <link
+              href="/favicons/apple-touch-icon.png"
+              rel="apple-touch-icon"
+              sizes="180x180"
+            />
+            <link
+              href="/favicons/favicon-32x32.png"
+              rel="icon"
+              sizes="32x32"
+              type="image/png"
+            />
+            <link
+              href="/favicons/favicon-16x16.png"
+              rel="icon"
+              sizes="16x16"
+              type="image/png"
+            />
+            <link href="/favicons/site.webmanifest" rel="manifest" />
+            <link href="/favicons/favicon.ico" rel="icon" />
+          </Head>
+          <UserProvider>
+            <Navigation />
+            <LazyMotion features={loadFramerMotionFeatures} strict>
+              <Component {...pageProps} />
+            </LazyMotion>
+          </UserProvider>
+          <Script
+            strategy="lazyOnload"
+            src="https://status.skylarkplatform.com/embed/script.js"
           />
-          <link
-            href="/favicons/apple-touch-icon.png"
-            rel="apple-touch-icon"
-            sizes="180x180"
-          />
-          <link
-            href="/favicons/favicon-32x32.png"
-            rel="icon"
-            sizes="32x32"
-            type="image/png"
-          />
-          <link
-            href="/favicons/favicon-16x16.png"
-            rel="icon"
-            sizes="16x16"
-            type="image/png"
-          />
-          <link href="/favicons/site.webmanifest" rel="manifest" />
-          <link href="/favicons/favicon.ico" rel="icon" />
-        </Head>
-        <UserProvider>
-          <Navigation />
-          <LazyMotion features={loadFramerMotionFeatures} strict>
-            <Component {...pageProps} />
-          </LazyMotion>
-        </UserProvider>
-        <Script
-          strategy="lazyOnload"
-          src="https://status.skylarkplatform.com/embed/script.js"
-        />
-      </QueryClientProvider>
+        </QueryClientProvider>
+      </IntercomProvider>
     </PlausibleProvider>
   );
 }
