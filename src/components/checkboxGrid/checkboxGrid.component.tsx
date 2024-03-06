7d050e5a3eab99e6215823d04f56cd6ba54bc1ea
James Wallis <j@wallis.dev>
2024-03-06 18:05:03 +0100

    feat: tweak css to limit columns when less than 6 checkboxes exist (#244)

--- src/components/checkboxGrid/checkboxGrid.component.tsx
+++ src/components/checkboxGrid/checkboxGrid.component.tsx
@@ -67,7 +67,7 @@ export const CheckboxGridToggleAll = ({
   return (
     <Checkbox
       label="Toggle all"
-      className="mb-2"
+      className="mb-1"
       onCheckedChange={handleToggleAll}
       name={name}
       checked={allOptionsChecked}
@@ -116,12 +116,30 @@ export const CheckboxGrid = ({
           checkedOptions={checkedOptions}
         />
       )}
-      <div className="columns-2 space-y-2 md:columns-4 lg:columns-5 xl:columns-6">
+      <div
+        className={clsx(
+          // Safari seems to have a bug where if you have blank columns, they do a weird half overflow
+          // So if you have 4 checkboxes but are using columns-6, the 4 checkboxes will cut in half
+          // due to this we limit the number of columns and then offset width when there is less than 6 checkboxes
+          options.length === 1 && "columns-1",
+          options.length >= 2 && "columns-2",
+          options.length === 3 && "md:columns-3",
+          options.length >= 4 && "md:columns-4",
+          options.length >= 5 && "lg:columns-5",
+          options.length >= 6 && "xl:columns-6",
+          options.length === 1 && "md:w-1/4 lg:w-1/5 xl:w-1/6",
+          options.length === 2 && "md:w-2/4 lg:w-2/5 xl:w-2/6",
+          options.length === 3 && "md:w-3/4 lg:w-3/5 xl:w-3/6",
+          options.length === 4 && "lg:w-4/5 xl:w-4/6",
+          options.length === 5 && "xl:w-5/6",
+        )}
+      >
         {options.map(({ option }) => (
           <Checkbox
             label={option.label}
             key={option.value}
             name={`${label}-${option.value}`}
+            className="my-0.5"
             checked={checkedOptions.includes(option.value)}
             onCheckedChange={(checkedState) =>
               handleChange(option, checkedState)
