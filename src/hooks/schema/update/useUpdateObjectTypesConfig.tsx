da97a6e5ba809a79d9fd09b51b5d8544be71bad2
James Wallis <j@wallis.dev>
2024-12-13 16:31:53 +0000

    feat: add functionality to create new object type

--- src/hooks/schema/update/useUpdateObjectTypesConfig.tsx
+++ src/hooks/schema/update/useUpdateObjectTypesConfig.tsx
@@ -11,11 +11,8 @@ import {
 import { QueryKeys } from "src/enums/graphql";
 import {
   GQLSkylarkErrorResponse,
-  NormalizedObjectFieldType,
-  SkylarkObjectFieldType,
   SkylarkGraphQLObjectConfigFieldConfig,
 } from "src/interfaces/skylark";
-import { SchemaVersion } from "src/interfaces/skylark/environment";
 import { skylarkRequest } from "src/lib/graphql/skylark/client";
 import { wrappedJsonMutation } from "src/lib/graphql/skylark/dynamicQueries";
 
@@ -24,84 +21,6 @@ interface MutationArgs {
   modifiedFormFields: FormState<ContentModelEditorForm>["dirtyFields"];
 }
 
-interface EditFieldConfigurationField {
-  name: string;
-  operation: EnumType;
-  is_translatable: boolean;
-  required: boolean;
-  type: EnumType;
-  enum_name?: string;
-}
-
-const convertFieldType = (
-  type: NormalizedObjectFieldType,
-): SkylarkObjectFieldType => {
-  switch (type) {
-    case "int":
-      return "INTEGER";
-    case "ipaddress":
-      return "IP_ADDRESS";
-    default:
-      return type.toUpperCase() as SkylarkObjectFieldType;
-  }
-};
-
-// const parseFieldConfigurationField = (
-//   field: ContentModelEditorFormObjectTypeField,
-// ): EditFieldConfigurationField | null => {
-//   console.log({ field });
-//   if (field.isNew && field.isDeleted) {
-//     return null;
-//   }
-
-//   const type = convertFieldType(field.type);
-
-//   const common: Omit<EditFieldConfigurationField, "operation"> = {
-//     name: field.name,
-//     is_translatable: Boolean(field.isTranslatable),
-//     required: Boolean(field.isRequired),
-//     type: new EnumType(type),
-//   };
-
-//   if (type === "ENUM") {
-//     common["enum_name"] = field.originalType;
-//   }
-
-//   if (field.isNew) {
-//     return {
-//       ...common,
-//       operation: new EnumType("CREATE"),
-//     };
-//   }
-
-//   if (field.isDeleted) {
-//     return {
-//       ...common,
-//       operation: new EnumType("DELETE"),
-//     };
-//   }
-
-//   return null;
-// };
-// editFieldConfiguration(
-//   fields: {name: "synopsis", operation: UPDATE, is_translatable: false, required: false, type: INTEGER}
-//   object_class: LiveStream
-// ) {
-//   version
-// }
-
-// setObjectTypeConfiguration(
-//   object_type: Episode
-//   object_type_config: {display_name: "", field_config: {name: "test_field_no", ui_field_type: TEXTAREA, ui_position: 10}}
-// ) {
-//   display_name
-//   primary_field
-//   field_config {
-//     name
-//     ui_field_type
-//     ui_position
-//   }
-// }
 const buildEditObjectTypeConfigurationMutation = (
   objectType: string,
   fields: ContentModelEditorFormObjectTypeField[],
@@ -110,8 +29,6 @@ const buildEditObjectTypeConfigurationMutation = (
   if (fields.length === 0) {
     return null;
   }
-
-  console.log({ objectType, fields, uiConfig });
 
   return {
     key: `${objectType}_setObjectTypeConfiguration`,
@@ -155,23 +72,10 @@ const buildEditObjectTypeConfigurationMutation = (
   };
 };
 
-/**
- * No clue what this was... ???
- * Only support:
- * - Add fields
- * - Remove fields
- * - Add relationships
- * - Remove relationships
- * - Add object types
- *    - Add fields
- *    - Add relationships
- */
-const buildSchemaMutation = (
+const buildObjectTypesConfigMutation = (
   formValues: ContentModelEditorForm,
   modifiedObjectTypes: string[],
 ) => {
-  console.log({ modifiedObjectTypes });
-
   if (!modifiedObjectTypes || modifiedObjectTypes.length === 0) {
     return null;
   }
@@ -233,11 +137,10 @@ export const useUpdateObjectTypesConfiguration = ({
           ?.filter((objectType) => !formValues.objectTypes[objectType].isNew) ||
         [];
 
-      console.log("here", { modifiedObjectTypes, modifiedFormFields });
-
-      const mutation = buildSchemaMutation(formValues, modifiedObjectTypes);
-
-      console.log("updateObjectTypesConfiguration", { mutation });
+      const mutation = buildObjectTypesConfigMutation(
+        formValues,
+        modifiedObjectTypes,
+      );
 
       if (modifiedObjectTypes.length === 0 || !mutation) {
         return null;
